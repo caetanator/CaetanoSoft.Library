9122c799e3911e1a47cf5f3de5d44fe697b91f70
José Caetano Silva <caetanator@users.noreply.github.com>
2026-04-30 10:17:22 +0100

    Updates

--- src/main/java/CaetanoSoft/Utilities/OS/EnumCodesOS.java
+++ src/main/java/CaetanoSoft/Utilities/OS/EnumCodesOS.java
@@ -26,6 +26,8 @@
 //          along with CaetanoSoft.Library. If not, see 
 //          <https://www.gnu.org/licenses/gpl-3.0.html>.
 //******************************************************************************
+
+
 package CaetanoSoft.Utilities.OS;
 
 /**
